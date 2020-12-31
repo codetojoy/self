04e3514444612571d2d2b8b339df9f43446ac86c
Michael Easter <codetojoy@gmail.com>
2020-12-31 13:57:21 -0400

    incremental update Thu 31 Dec 2020 13:57:21 AST

--- personal/fitness/data/concat.sh
+++ personal/fitness/data/concat.sh
@@ -6,4 +6,8 @@ cat 2006_fitness.csv 2007_fitness.csv 2008_fitness.csv \
     2009_fitness.csv 2010_fitness.csv 2011_fitness.csv \
     2012_fitness.csv 2013_fitness.csv 2014_fitness.csv \
     2015_fitness.csv 2016_fitness.csv 2017_fitness.csv \
-    2018_fitness.csv 2019_fitness.csv 2020_fitness.csv  > all_fitness.csv 
+    2018_fitness.csv 2019_fitness.csv 2020_fitness.csv  > tmp.csv 
+
+grep -v "Date.*Run.*Bike.*Swim.*Weight.*Notes" tmp.csv > all_fitness.csv
+
+rm tmp.csv
