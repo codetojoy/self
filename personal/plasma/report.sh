de66edb999e2ed47e27ce58ee11f4e928c3a3c03
Michael Easter <codetojoy@gmail.com>
2021-01-14 05:57:38 -0400

    incremental update Thu 14 Jan 2021 05:57:38 AST

---
+++ personal/plasma/report.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+MY_FILE=./plasma.csv
+
+echo "# left arm: `grep LEFT $MY_FILE | wc -l`" 
+echo "# right arm: `grep RIGHT $MY_FILE | wc -l`" 
+echo ""
+echo "# Two-Draw McGraw: `grep ",\\"2\\"," $MY_FILE | wc -l`"
+echo ""
+echo "# 2021: `grep 2021 $MY_FILE | wc -l`"
+echo "# 2020: `grep 2020 $MY_FILE | wc -l`"
+echo "# 2019: `grep 2019 $MY_FILE | wc -l`"
+echo "# total: `grep -v "date.*draws" $MY_FILE | wc -l`" 
+
