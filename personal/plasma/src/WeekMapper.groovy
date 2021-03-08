6f5056686713b1c452e1bb80e7b5e7b50fc53c52
Michael Easter <codetojoy@gmail.com>
2021-03-07 20:26:11 -0400

    incremental update Sun  7 Mar 2021 20:26:11 AST

--- personal/plasma/src/WeekMapper.groovy
+++ personal/plasma/src/WeekMapper.groovy
@@ -9,7 +9,6 @@ import org.joda.time.*
 def formatter = DateTimeFormat.forPattern("dd-MMM-yyyy")
 def regex = /^"(.*?)".*$/
 
-def isHeader = true
 
 def processLine = { def line ->
     def matcher = line =~ regex
@@ -23,7 +22,15 @@ def processLine = { def line ->
 
 // ------------------ main 
 
-new File(args[0]).eachLine { def line ->
+if (args.length == 0) {
+    System.err.println "usage: need CSV file (e.g. plasma.csv)"
+    System.exit(-5150)
+}
+
+def isHeader = true
+def file = args[0]
+
+new File(file).eachLine { def line ->
     if (!isHeader) {
         processLine(line)
     } else {
